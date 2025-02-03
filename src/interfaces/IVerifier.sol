7e12656679755563a70254afea2800e1affa6085
vdrg <vdrg@protonmail.ch>
2025-02-03 19:01:36 -0300

    Fix struct encoding

--- src/interfaces/IVerifier.sol
+++ src/interfaces/IVerifier.sol
@@ -5,13 +5,13 @@ struct JwtProof {
     uint256 kid; // Domain name of the sender's email
     string iss;
     string azp;
-    string domainName;
     bytes32 publicKeyHash; // Hash of the DKIM public key used in email/proof
     uint256 timestamp; // Timestamp of the email
     string maskedCommand; // Masked command of the email
     bytes32 emailNullifier; // Nullifier of the email to prevent its reuse.
     bytes32 accountSalt; // Create2 salt of the account
     bool isCodeExist; // Check if the account code is exist
+    string domainName;
     bytes proof; // ZK Proof of Email
 }
 
